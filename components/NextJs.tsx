25231174efe7a09168de9a650045e84f9c6c29c4
Aleksander Kostopoulos <aleksander.kostopoulos@gmail.com>
2025-09-19 12:18:14 +0200

    Gjort klart next component og mappet ut

---
+++ components/NextJs.tsx
@@ -0,0 +1,31 @@
+import { showNextSection } from "@/data/info";
+
+export default function NextJs() {
+  const data = showNextSection();
+  return (
+    <section className="section-component">
+      {data.map((sec) => (
+        <article key={sec.id} className="article-component">
+          <h2>{sec.title}</h2>
+
+          {sec.text.map((p) => (
+            <p key={p}>{p}</p>
+          ))}
+
+          {sec.code && (
+            <figure className="figure">
+              {sec.code.caption && (
+                <figcaption className="caption">{sec.code.caption}</figcaption>
+              )}
+              <pre className="pre">
+                <code className={`language-${sec.code.lang}`}>
+                  {sec.code.snippet}
+                </code>
+              </pre>
+            </figure>
+          )}
+        </article>
+      ))}
+    </section>
+  );
+}
