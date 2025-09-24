6a843d7c9c0d98ebda2278200fc5fb953c217f92
melz3r <ola.letrud@gmail.com>
2025-09-24 17:56:48 +0200

    endret størrelse på logo i heaader.txt for at header skal ta mindre høyde

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -15,16 +15,13 @@ export default function Header() {
           <Image
             src="/web-wizard-logo.svg"
             alt="Web Wizard Logo"
-            width={400}
-            height={130}
+            width={300}
+            height={98}
             className={styles.logoImg}
           />
         </Link>
       </div>
-      <button
-        className={styles.menuButton}
-        onClick={() => setOpen((v) => !v)}
-      >
+      <button className={styles.menuButton} onClick={() => setOpen((v) => !v)}>
         <span className={open ? styles.iconClose : styles.iconBurger} />
       </button>
       <nav
@@ -33,9 +30,15 @@ export default function Header() {
         onClick={() => setOpen(false)}
       >
         <ul>
-          <li><Link href="/">Forside</Link></li>
-          <li><Link href="/oppgaver">Quiz</Link></li>
-          <li><Link href="/fagstoff">Fagstoff</Link></li>
+          <li>
+            <Link href="/">Forside</Link>
+          </li>
+          <li>
+            <Link href="/oppgaver">Quiz</Link>
+          </li>
+          <li>
+            <Link href="/fagstoff">Fagstoff</Link>
+          </li>
         </ul>
       </nav>
     </header>
