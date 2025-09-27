add3fe1246899febc4ae3a6f3f61be8e4d34bdf7
Aleksander Kostopoulos <aleksander.kostopoulos@gmail.com>
2025-09-27 11:22:32 +0200

    Fjernet font som ikke ble brukt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 import Footer from "@/components/common/Footer";
-import { Inter, Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
@@ -8,15 +8,10 @@ const inter = Inter({
   weight: ["200", "400", "700"],
   variable: "--font-inter",
 });
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-roboto",
-});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="no" className={`${inter.variable} ${roboto.variable}`}>
+    <html lang="no" className={`${inter.variable}`}>
       <body>
         {children}
         <Footer />
